4d44764489afd3441fb5e5bd60a5e129bf0f8877
jonashering <jnshering@gmail.com>
2019-11-28 10:51:23 +0100

    add kafka skeleton from demo

--- src/main/java/annoyED/App.java
+++ src/main/java/annoyED/App.java
@@ -3,12 +3,76 @@
  */
 package annoyED;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static final String INPUT_TOPIC = "streams-point-input";
+    public static final String OUTPUT_TOPIC = "streams-nearestneighbor-output";
+    public static final String STORE_NAME = "CountsKeyValueStore";
+
+    static Properties getStreamsConfig() {
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-nearestneighbor");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return props;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    static void createNearestNeighborStream(final StreamsBuilder builder) {
+        final KStream<String, String> source = builder.stream(INPUT_TOPIC);
+
+        final KTable<String, Long> counts = source
+                .flatMapValues(value -> Arrays.asList(value.toUpperCase(Locale.getDefault()).split(" ")))
+                .groupBy((key, value) -> value)
+                .count(Materialized.as(STORE_NAME));
+
+        // need to override value serde to Long type
+        counts.toStream().to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+    }
+
+    public static void main(final String[] args) {
+        final Properties props = getStreamsConfig();
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        createNearestNeighborStream(builder);
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-wordcount-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (final Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 }
--- src/test/java/annoyED/AppTest.java
+++ src/test/java/annoyED/AppTest.java
@@ -3,12 +3,56 @@
  */
 package annoyED;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.streams.test.OutputVerifier;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static annoyED.App.*;
+
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
+    private TopologyTestDriver testDriver;
+    private StringDeserializer stringDeserializer = new StringDeserializer();
+    private LongDeserializer longDeserializer = new LongDeserializer();
+    private ConsumerRecordFactory<String, String> recordFactory = new ConsumerRecordFactory<>(new StringSerializer(), new StringSerializer());
+
+    @Before
+    public void setup() {
+        final StreamsBuilder builder = new StreamsBuilder();
+        App.createNearestNeighborStream(builder);
+        testDriver = new TopologyTestDriver(builder.build(), App.getStreamsConfig());
     }
+
+    @After
+    public void tearDown() {
+        try {
+            testDriver.close();
+        } catch (final RuntimeException e) {
+            System.out.println("Ignoring exception, test failing in Windows due this exception:" + e.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    public void testOneWord() {
+        testDriver.pipeInput(recordFactory.create(INPUT_TOPIC, null, "Hello"));
+
+        ProducerRecord<String, Long> outputRecord = testDriver.readOutput(
+                OUTPUT_TOPIC,
+                stringDeserializer,
+                longDeserializer);
+
+        OutputVerifier.compareKeyValue(outputRecord, "hello", 1L);
+        assertNull(testDriver.readOutput(OUTPUT_TOPIC, stringDeserializer, longDeserializer));
+    }
+
 }
